4a38aa33ba12b9a0513761df28ab62c4206c0493
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-01-22 20:11:54 -0300

    the function dropItem was added with the functionality of drop an item according with its id

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -14,7 +14,7 @@ function App() {
     const task = todoTaskRef.current.value;
     if(task==="")return;
     setTodos((prevTodos)=>{
-      return [...prevTodos, {id:prevTodos.length+1, task, completed:false}]
+      return [...prevTodos, {id:prevTodos[(prevTodos.length-1)].id+1, task, completed:false}]
     });
     
     todoTaskRef.current.value=null;
@@ -27,12 +27,22 @@ function App() {
     setTodos(newTodos);
   }
 
+  const dropItem = ()=> 
+  {
+    if (isNaN(todoTaskRef.current.value)) return;
+    const idItem= todoTaskRef.current.value;
+    let newTodos=[...todos];
+    const indexOfItem=idItem-1
+    newTodos.splice(indexOfItem,1);
+    setTodos(newTodos);
+  }
+
   return (  
     <React.Fragment>
       <ToDoList todos={todos} toggleTodo={toggleTodo}/>
       <input ref={todoTaskRef} type="text" placeholder='Add a new task or drop by id'/>
       <button onClick={handleTodoAdd}>✅</button>
-      <button>🗑</button>
+      <button onClick={dropItem}>🗑</button>
     </React.Fragment>
   );
 }
