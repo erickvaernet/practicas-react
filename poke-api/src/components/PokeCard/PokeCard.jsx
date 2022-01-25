3eb0d81a5b8f7893e57db5e4c2fdbec2c3d765b0
Erick Adriel Vaernet <erickadrielnet@gmail.com>
2022-01-25 13:29:58 -0300

    PokeCard was created and now have de functionality of call an api and return a card with the pokemon info

---
+++ poke-api/src/components/PokeCard/PokeCard.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from "react";
+
+
+export function PokeCard({id}){
+
+    const [pokemon,setPokemon]=useState({name:null, abilities: null, sprite:null});
+
+    const url= "https://pokeapi.co/api/v2/pokemon/"+id;
+
+    useEffect(()=>{
+        fetch(url)
+        .then((res)=> res.json())
+        .then((data)=>{
+            console.log(data)
+            const pokemonName=data.name;
+            const pokemonSprite=data.sprites.front_default;            
+            const pokemonAbilities= data.abilities.reduce( (prev,current) => prev.ability.name+"-"+current.ability.name);
+            setPokemon({name:pokemonName, abilities:pokemonAbilities, sprite:pokemonSprite});
+        })
+    },[])
+
+    
+
+    return(
+        <div className="Marco">
+            <h2>{pokemon.name}</h2>
+            <img src={pokemon.sprite} alt={pokemon.name} />
+            <p>{pokemon.abilities}</p>
+        </div>        
+    );
+}
---
+++ poke-api/src/components/button/ButtonStd.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export function ButtonStd({src,alt}){      
+      
+      return(            
+            <button>
+                  <img src={src} alt={alt} />
+            </button>
+      )
+}
